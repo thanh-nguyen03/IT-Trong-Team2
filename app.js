16ccbbda39fb5837177bef29469752dd7d4defc1
thanh-nguyen03 <nguyenducthanh240903@gmail.com>
2021-12-22 09:27:45 +0700

    yeahh

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// Check mobile user
-/* Storing user's device details in a variable*/
-let details = navigator.userAgent;
-
-/* Creating a regular expression 
-containing some mobile devices keywords 
-to search it in details string*/
-let regexp = /android|iphone|kindle|ipad/i;
-
-/* Using test() method to search regexp in details
-it returns boolean value*/
-let isMobileDevice = regexp.test(details);
-
-if (isMobileDevice) {
-  alert('Vui lòng xem bằng trình duyệt máy tính để có trải nghiệm tốt nhất <3');
-}
-// alert('Vui lòng xem bằng trình duyệt máy tính để có trải nghiệm tốt nhất <3');
-
 // Animation
 let sub = document.getElementById('sub_hover');
 const hover = () => {
